b84f732c9b67dc47fd1d4e0332dded513473e4c8
Texas Hedge'Em <github@mantura.com>
2022-04-19 11:46:14 +0100

    Added OpenBanking Testing as a project and corrected some typos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,23 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Open Banking - Live Testing Service',
+    subtitle: 'A service that help Open Banking API and API develops get access to live bank accounts to help with testing. ',
+    image: '/images/projects/consentwise.png',
+    date: '2022-05-1',
+    desc:
+      ' This service addresses and Open Banking industry wide problem - how to test your APP of API with real/live accounts. '
+      + 'See http://qeetoto.com',
+  },
+  {
     title: 'React Demo - Personal Profile',
     subtitle: 'Developer on React, Auto hosted on Git Pages .',
     image: '/images/projects/personal-profile.png',
     date: '2022-04-1',
     desc:
-      'Created this React personal profile using a template from forked from MICHAEL D`ANGELO'
-      + 'Create to demo speed I can pick up new technolgies.'
-      + 'I learn through doing - typically by following in the footsteps of others to not re-invent the wheel',
+      'Created this React personal profile using a template from forked from MICHAEL D`ANGELO. '
+      + 'Create to demo speed I can pick up new technolgies. '
+      + 'I learn through doing - typically by following in the footsteps of others so as to not re-invent the wheel.',
   },
   {
     title: 'Texas HedgeEm',
@@ -16,10 +25,12 @@ const data = [
     image: '/images/projects/texas-hedgeem.png',
     date: '2015-05-15',
     desc:
-      'Designed, developed, implemented, branded, promoted and hosted Texas Hedge’Em® an online card game based on the mathematically probability of events occurring. Submitted US Patent and secured UK Trade Mark. Presented to multiple C-level executives securing development on professional platform'
-      + '£2m in place bets in first year.'
-      + 'Still live in many online casinos'
-      + 'I will not be retiring on this - the industry is fierce - read more in my post about this.',
+      'Designed, developed, implemented, branded, promoted and hosted Texas Hedge’Em® an online card game based '
+      + 'on the mathematically probability of events occurring. Submitted US Patent and secured UK Trade Mark. '
+      + 'Presented to multiple C-level executives securing development on professional platform. '
+      + 'Grossed £2m in placed bets in first year. '
+      + 'The game is still enjoyed by players in many online casinos (see https://slotcatalog.com/en/slots/Texas-Hedge-Em for list).  '
+      + 'Unfortunately, as great as this game is, I will not be retiring on it; the industry is fierce - read more in my post about this.',
   },
   {
     title: 'Schemation',
@@ -27,12 +38,12 @@ const data = [
     image: '/images/projects/schemation.jpg',
     date: '2002-05-15',
     desc:
-      'Built a full CI/CD DevOps pipeline before this terms even existed '
-      + 'The original product supported an 80+ development team that build the original .gov.uk website'
-      + 'Every developer had there own environment including an Oracle schema - which was quiet novel at the time'
-      + 'Whilst the project was a technical success, it was a commercial failure as we the four founders spent too much effort on the tech and not enough on the commercials'
-      + 'We have retired the product now, but it is still available to anyone who asks.'
-      + 'Fun fact: We called our early builds Batman builds because they build over night! ',
+      'Built a full CI/CD DevOps pipeline before this terms even existed.  '
+      + 'The original product supported an 80+ development team that built the original .gov.uk website. '
+      + 'Every developer had there own environment including an Oracle schema - which was quite novel at the time. '
+      + 'Whilst the project was a technical success, it was a commercial failure as we the four founders spent too much effort on the tech and not enough on the commercials. '
+      + 'We have retired the product now, but it is still available to anyone who asks. '
+      + 'Fun fact: We called our early builds "Batman Builds" because they built over night! ',
   },
 ];
 
