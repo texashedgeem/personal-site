052eb84bbc4d4d2191d288529cad2c9ada32435e
qeetoto_showcase <atlassian@qeetoto.com>
2022-10-27 12:46:22 +0100

    Changed certificate to link to pdf to make more easily downloadable from simonhewins.co.uk website

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,32 +1,37 @@
 const courses = [
   {
     title: 'ITIL® Foundation certificate in IT Service Management',
-    number: '00334871',
-    link: 'https://eprofessional.bcs.org/Portal/Candidate/Results/GetECertificate/574742',
-    university: 'BCS - The Chartered Institute for IT',
-  },
-  {
-    title: 'ITIL®Intermediate certificate in IT Service Operation',
-    number: '00345591',
-    link: 'https://eprofessional.bcs.org/Portal/Candidate/Results/GetECertificate/596074',
-    university: 'BCS - The Chartered Institute for IT',
-  },
-  {
-    title: 'ITIL® Intermediate certificate in IT Service Design',
-    number: '00358022',
-    link: 'https://eprofessional.bcs.org/Portal/Candidate/Results/GetECertificate/624972',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/ITIL_Foundation.pdf',
     university: 'BCS - The Chartered Institute for IT',
   },
   {
     title: 'ITIL® Intermediate certificate in IT Service Strategy',
     number: '00375235',
-    link: 'https://eprofessional.bcs.org/Portal/Candidate/Results/GetECertificate/656191',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/ITIL_ServiceStrategyIntermediate.pdf',
+    university: 'BCS - The Chartered Institute for IT',
+  },
+  {
+    title: 'ITIL® Intermediate certificate in IT Service Design',
+    number: '00358022',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/ITIL_ServiceDesignIntermediate.pdf',
+    university: 'BCS - The Chartered Institute for IT',
+  },
+  {
+    title: 'ITIL®Intermediate certificate in IT Service Operation',
+    number: '00345591',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/ITIL_ServiceOperationIntermediate.pdf',
     university: 'BCS - The Chartered Institute for IT',
   },
   {
     title: 'ITIL® Intermediate certificate in IT Continual Service Improvement',
     number: '00393360',
-    link: 'https://eprofessional.bcs.org/Portal/Candidate/Results/GetECertificate/685239',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/ITIL_CSI_Intermediate.pdf',
+    university: 'BCS - The Chartered Institute for IT',
+  },
+  {
+    title: 'Microsoft Certified Azure Fundamentals',
+    number: '00393360',
+    link: 'https://simonhewins.co.uk/CERTIFICATES/Microsort_Azure_Fundamentals.pdf',
     university: 'BCS - The Chartered Institute for IT',
   },
 ];
